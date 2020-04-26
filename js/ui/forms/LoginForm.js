e36bfa93d7f8ae9167b40aee8e225dbbc643826e
Iva Kochneva <inkybasik@gmail.com>
2020-04-26 18:01:20 +0300

    Is LoginForm.js done?

--- js/ui/forms/LoginForm.js
+++ js/ui/forms/LoginForm.js
@@ -3,7 +3,7 @@
  * входа в портал
  * Наследуется от AsyncForm
  * */
-class LoginForm {
+class LoginForm extends AsyncForm {
   /**
    * Производит авторизацию с помощью User.login
    * После успешной авторизации, сбрасывает форму,
@@ -11,6 +11,17 @@ class LoginForm {
    * закрывает окно, в котором находится форма
    * */
   onSubmit( options ) {
-
+    User.login(options, (err, response) => {
+      if(response) {
+        let formData = new FormData(myForm);
+        for (let entry of formData.entries()) {
+          formData.delete(entry[0]);
+        };
+        App.setState( 'user-logged' );
+      } else {
+        console.log(err);
+      }
+    });  
+    this.element.closest('.modal').close();
   }
 }
