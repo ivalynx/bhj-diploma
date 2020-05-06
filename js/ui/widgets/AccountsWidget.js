2fd8152b2e7ecfbfaabeb2755b1f5d9270a2daaf
Iva Kochneva <inkybasik@gmail.com>
2020-05-06 10:22:27 +0300

    Удалила ненужные теперь console.log

--- js/ui/widgets/AccountsWidget.js
+++ js/ui/widgets/AccountsWidget.js
@@ -55,8 +55,6 @@ class AccountsWidget {
         if(response.success) {
           this.clear();
           this.renderItem(response.data);
-          console.log('Account.list response:')
-          console.log(response);
         } else {
           console.log(err)
         };
